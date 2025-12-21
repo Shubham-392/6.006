b7c8f7d5a59dbdbec1c75fc05924fa473cf39911
Shubham <123jangrashubham@gmail.com>
2025-12-21 20:38:27 +0530

    #add implemented Selection sorting

---
+++ data_structure/Interfaces/set/sorting/selection_sort.py
@@ -0,0 +1,16 @@
+## Selection Sort Example
+############################
+def selection_sort(L, detail = False):
+    for i in range(len(L)):
+        for j in range(i, len(L)):
+            if L[j] < L[i]:
+                L[i], L[j] = L[j], L[i]
+            if detail == True:
+                print(L)
+        print()
+
+# print("--- SELECTION SORT ---")
+# L = [8, 4, 1, 6, 5, 11, 2, 0]
+# print('L:       ', L)
+# selection_sort(L, True)
+# print('Sorted L:', L)
